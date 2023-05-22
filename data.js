0cee15fe3cfc18c64bb3d3b0d2d589acde4e90bf
vinodhconnects <writevinodh@gmail.com>
2023-05-22 12:54:39 +0530

    array

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 //Javascript is an intepreted language
 // Javascript is not a strictly typed language [No Data types definition before hand]
-// Javascript supports number, string and boolean scalar types
+// Javascript supports number, string and boolean types
 // literals  - for boolean  we true for true, false for false
 // literals - for string we can either enclose the string with single or double quote
 // variables that are declared in a js file directly not part of functions is
@@ -16,3 +16,5 @@ console.log("a's data type: ", typeof(a))
 console.log("b's data type: ", typeof(b))
 console.log("c's data type: ", typeof(c))
 console.log("d's data type: ", typeof(d))
+
+
---
+++ objecttypes.js
@@ -0,0 +1,13 @@
+//apart from scalar types, javascript supports objects and 
+//array type of data
+
+//javascript array
+//array is collection of js objects
+
+a=[12,3453,34,34,35]
+b=[34,"Ram",true,"Peter"]
+
+console.log(a)
+console.log(b)
+//indexing starts with 0
+console.log(b[3])
