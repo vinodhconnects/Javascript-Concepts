aed316b13d068830b4340bc576a1d6fad0cc5107
vinodhconnects <writevinodh@gmail.com>
2023-05-22 12:46:03 +0530

    datatype

---
+++ data.js
@@ -0,0 +1,18 @@
+//Javascript is an intepreted language
+// Javascript is not a strictly typed language [No Data types definition before hand]
+// Javascript supports number, string and boolean scalar types
+// literals  - for boolean  we true for true, false for false
+// literals - for string we can either enclose the string with single or double quote
+// variables that are declared in a js file directly not part of functions is
+    //considered global, it will be available through out the file
+
+a=90  // no data type specification a is variable 90 is numeric literal
+b=false
+c="ram"
+d=90.43
+
+console.log(a,b,c,d)
+console.log("a's data type: ", typeof(a))
+console.log("b's data type: ", typeof(b))
+console.log("c's data type: ", typeof(c))
+console.log("d's data type: ", typeof(d))
