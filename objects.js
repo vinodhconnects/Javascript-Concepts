37b68a6f0668eb92ba95fe07a1d31257a15f0be6
vinodhconnects <writevinodh@gmail.com>
2023-05-23 11:52:13 +0530

    array-n-indexes

--- objects.js
+++ objects.js
@@ -4,8 +4,13 @@ person = {
     city: "Mumbai"
 }
 
+list = [34,345,345]
+list["sno"]=35
+list["name"]="Arjun"
 for(let  x in person)
     console.log(x, "=>", person[x])
 
 console.log(Object.keys(person))
+console.log(Object.keys(list))
 
+ 
