26a8bd9f8e23b54c701889ed173bea1adbf4971b
vinodhconnects <writevinodh@gmail.com>
2023-05-23 12:38:44 +0530

    spread-change

--- spread.js
+++ spread.js
@@ -1,7 +1,11 @@
-c=[34,12,2455]
-a=[3435,34,34,656,35,c]
-
-
+c=[956,12,2455]
+a=[3435,34,34,656,35,[...c]]
+console.log(a)
+b=[...a]
+c[3]=9090
+console.log(a)
+console.log(b)
+/*
 b=[...a] //copies the value of the array, not reference
 b[5]=[...a[5]]
 //es2015 and later
@@ -10,4 +14,4 @@ console.log(a)
 console.log(b)
 b[8]=9090
 console.log(a)
-console.log(b)
+console.log(b)***/
