e24d632e8b5dcc0da37a17283e5e7ba1a672e612
vinodhconnects <writevinodh@gmail.com>
2023-05-23 12:10:43 +0530

    array-reference

--- objects.js
+++ objects.js
@@ -4,13 +4,17 @@ person = {
     city: "Mumbai"
 }
 
+person.skills = ["Java","CSS","JAvascript"]
 list = [34,345,345]
 list["sno"]=35
 list["name"]="Arjun"
+console.log(person)
 for(let  x in person)
     console.log(x, "=>", person[x])
 
 console.log(Object.keys(person))
 console.log(Object.keys(list))
+
+
 
  
---
+++ references.js
@@ -0,0 +1,8 @@
+a = [45,466,199]
+
+console.log(a)
+
+b=a // pass by reference
+console.log(b)
+b[4]=900
+console.log(a)
