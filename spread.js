afcca6bc5a6b777706e58721a8e0280cf89baaf3
vinodhconnects <writevinodh@gmail.com>
2023-05-23 13:01:01 +0530

    questoin

--- spread.js
+++ spread.js
@@ -1,8 +1,11 @@
 c=[956,12,2455]
 a=[3435,34,34,656,35,[...c]]
 console.log(a)
-b=[...a]
+b=[...a[5]] // b=[956,12,2455]
+a[1]= 777 // only a will be impacted , no impact on b
 c[3]=9090
+//c=[...a]
+b[5][2]=9098
 console.log(a)
 console.log(b)
 /*
