727f3c04449936074eefff5e2fcda39a43bcc71d
vinodhconnects <writevinodh@gmail.com>
2023-05-23 10:51:23 +0530

    array-n-indexes

--- array.js
+++ array.js
@@ -4,7 +4,15 @@ console.log(a[5])  // undefined
 console.log(a[234]) // undefined
 
 a[2]=90 //legal, works
-a[90]=890  //legal
+a[945]=890  //legal
 
 console.log(a)
 console.log(a[87])
+
+a[-23] = 3489
+a['place'] ="chennai"
+b={sno:1,name:"Lokesh"}
+a[b] = " Javascript Object"
+
+console.log(a.length , " is the size of array")
+console.log(a)
