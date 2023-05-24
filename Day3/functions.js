cd6ebbf5bdf534f53fdc58a182b6f7137ffcfb9d
vinodhconnects <writevinodh@gmail.com>
2023-05-24 10:59:57 +0530

    function-first

---
+++ Day3/functions.js
@@ -0,0 +1,17 @@
+// function can carry parameters
+// it is not mandatory to return values in a function
+// we wont specify return type in the function specification
+// as we dont have datatypes in js
+
+function firstfun(a,b) {
+    console.log(a,b)
+ }  // function  definition
+
+   
+firstfun(12,24) //function call
+firstfun()
+firstfun(123)
+firstfun(23,435,353)
+firstfun("Run","Fast","faster")
+firstfun(17,90)
+firstfun(78)
