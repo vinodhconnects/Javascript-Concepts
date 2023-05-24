006bce1a20eaeef98c0c97a40642110fd2b7edfc
vinodhconnects <writevinodh@gmail.com>
2023-05-24 12:43:51 +0530

    returningfunction

--- Day3/functionRfunction.js
+++ Day3/functionRfunction.js
@@ -1,4 +1,15 @@
-function sample(option) {
+function add (a,b) { return a+b}
+function sub(a,b) { return a-b}
+function sample(option)
+  {
+    if(option==1)
+       return sub
+    else
+       return add
+  }
+
+  console.log(sample(1)(43,35))
+/*function sample(option) {
     console.log("sample logic")
    if(option==1)
     return function(a,b){return a-b}
@@ -12,3 +23,4 @@ function sample(option) {
 
 //console.log(sample())
 console.log(sample(1)(12,35))
+*/
