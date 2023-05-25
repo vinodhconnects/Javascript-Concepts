605fbf04c5738fda0b97c8fc8718dec13150cfdf
vinodhconnects <writevinodh@gmail.com>
2023-05-25 11:03:47 +0530

    constant

--- Day4/VariableKeyword6.js
+++ Day4/VariableKeyword6.js
@@ -1,10 +1,10 @@
 
 
 function demoVariable (){
-    a=30
+   const a=30
     console.log("Within the outer range a is",a)
     {
-       let a=90 //error local scope is already declared
+        let a=90 //error local scope is already declared
         console.log("within the block range a is",a)
         
        
