368ad5d4f04b3ecf22c4fed6009e70b2a3a94090
vinodhconnects <writevinodh@gmail.com>
2023-05-25 11:56:58 +0530

    closure-further

--- Day4/ClosureAgain.js
+++ Day4/ClosureAgain.js
@@ -1,5 +1,5 @@
 function outer(){
-    a=30
+   var a=30
 
     function inner(){
         console.log("outer a value in inner",a)
@@ -9,6 +9,10 @@ function outer(){
 }
 
  fun = outer();
+ fun2 = outer()
  fun();  //30
  fun();  //31
  fun(); //32
+ fun2(); //33
+ fun()
+ fun2()
